9f69c86c57f1701c452e604340be3ad74314faf4
VolodymyK <114539981+VolodymyK@users.noreply.github.com>
2023-04-25 20:07:30 -0500

    made some updates for task 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,45 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+// Create and render gallery items
+const createGalleryItem = ({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}" data-original-img=${original}>
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>
+`;
+
+galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
+
+// Store instances in an array
+const instances = [];
+
+// Show corresponding instance when image is clicked
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+  const original = event.target
+    .closest(".gallery__link")
+    .getAttribute("data-original-img");
+  const instance = new SimpleLightbox(`<img src="${original}">`);
+  instances.push(instance); // Add instance to array
+  instance.open();
+  document.addEventListener("keydown", (event) => onEscPress(event, instance));
+});
+
+// Close instance on Esc key press
+const onEscPress = (event, instance) => {
+  const ESC_KEYCODE = "Escape";
+  if (event.code === ESC_KEYCODE) {
+    instance.close();
+    instances.splice(instances.indexOf(instance), 1); // Remove instance from array
+    document.removeEventListener("keydown", (event) =>
+      onEscPress(event, instance)
+    );
+  }
+};
--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,21 @@
+// Import Vimeo player library and lodash.throttle
+import Player from "@vimeo/player";
+import { throttle } from "lodash";
 
+// Initialize Vimeo player
+const player = new Player("vimeo-player");
+
+// Get the current playback time from local storage
+let currentTime = localStorage.getItem("videoplayer-current-time") || 0;
+
+// Set the playback time on the player
+player.setCurrentTime(currentTime);
+
+// Listen for time updates and save the current time to local storage
+player.on(
+  "timeupdate",
+  throttle(function (data) {
+    currentTime = data.seconds;
+    localStorage.setItem("videoplayer-current-time", currentTime);
+  }, 1000)
+);
