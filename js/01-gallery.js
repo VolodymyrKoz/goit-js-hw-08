f31a4f30011ceb170ec4b106f30cd3ae3c0ca95e
VolodymyK <114539981+VolodymyK@users.noreply.github.com>
2023-04-28 16:38:11 -0500

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,45 +1,18 @@
-// Описаний в документації
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from "./gallery-items.js";
-
 const galleryList = document.querySelector(".gallery");
-
 // Create and render gallery items
 const createGalleryItem = ({ preview, original, description }) => `
   <li class="gallery__item">
-    <a class="gallery__link" href="${original}" data-original-img=${original}>
+    <a class="gallery__link" href="${original}" data-caption="${description}" data-src=${original}>
       <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
   </li>
 `;
-
 galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
-
-// Store instances in an array
-const instances = [];
-
-// Show corresponding instance when image is clicked
-galleryList.addEventListener("click", (event) => {
-  event.preventDefault();
-  const original = event.target
-    .closest(".gallery__link")
-    .getAttribute("data-original-img");
-  const instance = new SimpleLightbox(`<img src="${original}">`);
-  instances.push(instance); // Add instance to array
-  instance.open();
-  document.addEventListener("keydown", (event) => onEscPress(event, instance));
+// Initialize SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "caption",
+  captionDelay: 250,
 });
-
-// Close instance on Esc key press
-const onEscPress = (event, instance) => {
-  const ESC_KEYCODE = "Escape";
-  if (event.code === ESC_KEYCODE) {
-    instance.close();
-    instances.splice(instances.indexOf(instance), 1); // Remove instance from array
-    document.removeEventListener("keydown", (event) =>
-      onEscPress(event, instance)
-    );
-  }
-};
