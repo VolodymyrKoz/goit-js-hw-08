e1bcdab21f3bb74f4e141410e3ba10409332767a
VolodymyK <114539981+VolodymyK@users.noreply.github.com>
2023-04-29 10:15:35 -0500

    update

--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,40 +1,59 @@
-import throttle from "lodash.throttle";
+// Імпортуємо бібліотеку lodash.throttle
+import throttle from "./lodash.throttle.js";
 
+import { throttle } from "lodash";
+
+// Usage:
+const throttledFunction = throttle(myFunction, 500);
+
+
+// Отримуємо елементи форми
 const form = document.querySelector(".feedback-form");
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 
-const updateLocalStorage = () => {
-  const formData = {
+// Ключ для збереження в локальному сховищі
+const STORAGE_KEY = "feedback-form-state";
+
+// Функція для збереження стану форми в локальне сховище
+const saveStateToLocalStorage = throttle(() => {
+  const state = {
     email: emailInput.value,
     message: messageInput.value,
   };
-  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+}, 500);
+
+// Функція для заповнення полів форми значеннями з локального сховища
+const setStateFromLocalStorage = () => {
+  const state = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (state) {
+    emailInput.value = state.email || "";
+    messageInput.value = state.message || "";
+  }
 };
 
-form.addEventListener("input", throttle(updateLocalStorage, 500));
+// Функція для очищення форми та локального сховища
+const resetFormAndLocalStorage = () => {
+  const state = {
+    email: "",
+    message: "",
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+  emailInput.value = "";
+  messageInput.value = "";
+  console.log(state);
+};
 
-const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+// Додаємо обробник події input до кожного поля форми
+emailInput.addEventListener("input", saveStateToLocalStorage);
+messageInput.addEventListener("input", saveStateToLocalStorage);
 
-if (savedData) {
-  emailInput.value = savedData.email;
-  messageInput.value = savedData.message;
-}
+// Перевіряємо стан локального сховища під час завантаження сторінки
+setStateFromLocalStorage();
 
-const onSubmit = (event) => {
+// Додаємо обробник події submit до форми
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const formData = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-
-  console.log(formData);
-
-  localStorage.removeItem("feedback-form-state");
-  emailInput.clear();
-  messageInput.clear();
-};
-
-form.addEventListener("submit", onSubmit);
-
+  resetFormAndLocalStorage();
+});
